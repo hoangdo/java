cdf55ff911ed2d0a2fd86da50225739ad6e90df7
Hoang Do <phoenixvn.book@gmail.com>
2013-06-15 19:13:57 +0200

    Initial setup

---
+++ em/web/rest-exporter/src/main/java/phx/em/web/App.java
@@ -0,0 +1,13 @@
+package phx.em.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
